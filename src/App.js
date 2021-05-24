1455e836b39c27b135f8ab2febf3558619c219dc
M_abedalnabi <abedalnabi96@gmail.com>
2021-05-24 16:12:36 +0300

    pulse 7+8

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import List from "./components/List";
 import ListItem from "./components/ListItem";
 
-const todos = [
-  { todo: "wake up", id: 1 },
-  { todo: "breakfast", id: 2 },
-  { todo: "study", id: 3 },
-];
 // jsx
 const App = () => {
-  return <h1>Hello World.</h1>;
+  const todos = [
+    { todo: "wake up", id: 1 },
+    { todo: "breakfast", id: 2 },
+    { todo: "study", id: 3 },
+  ];
+  return (
+    <>
+      <h1>Hello World.</h1>;
+      <List todoList={todos} />
+    </>
+  );
 };
 
 export default App;
--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-const List = () => {};
+const List = (props) => {
+  console.log(props);
+  const { todoList } = props;
+  return (
+    <>
+      <div className="todoList">
+        <ul>
+          {todoList.map((ele, i) => {
+            return <li></li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default List;
